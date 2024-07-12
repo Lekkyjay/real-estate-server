a48120aa760f49fd4d88c7a8b44212fe643b11a6
‘Lekky <lekkyjay@yahoo.com>
2024-07-12 18:14:04 +0200

    Refactored logger middleware.

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,8 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
 
-// GET http://localhost:5000/api/posts
 const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)  
+  const dateObj = new Date()
+
+  const date = ('0' + dateObj.getDate()).slice(-2)
+  const month = ('0' + (dateObj.getMonth() + 1)).slice(-2)
+  const year = dateObj.getFullYear()
+
+  const hours = dateObj.getHours()
+  const minutes = ('0' + dateObj.getMinutes()).slice(-2)
+  const seconds = ('0' + dateObj.getSeconds()).slice(-2)
+  
+  const fullDate = year + '-' + month + '-' + date + '::' + hours + ':' + minutes + ':' + seconds
+
+  // 2024-07-12::18:10:26  POST http://localhost:5000/api/auth/register
+  console.log(`${fullDate}  ${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)  
   next()
 }
 
