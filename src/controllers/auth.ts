92434a66fe80a8118ec44831e6a5b0f96ab509f4
‘Lekky <lekkyjay@yahoo.com>
2024-07-13 09:06:12 +0200

    Clean up.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,9 +7,7 @@ import CustomError from '../utils/customError'
 const register = async (req: Request, res: Response, next: NextFunction) => {
   const { username, email, password } = req.body
   const avatar = req.body.avatar ? req.body.avatar : 'https://res.cloudinary.com/dtkdchtfm/image/upload/v1658502077/products/yq4zj8vwtyofvh39mmlp.png'
-  // const avatarUrl = 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  console.log(req.body)
-
+  
   if (!email || !password || !username) return next(new CustomError(400, 'Missing credentials'))
 
   try {
